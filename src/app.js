0da7b4b81b2f1c2ac4b72250f18aa1e68ed974d8
Mayank Bajaj <mayankbajaj672@gmail.com>
2025-12-23 23:13:00 +0530

    user model and reister controller

--- src/app.js
+++ src/app.js
@@ -2,4 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json({ limit: "16kb" }))
+
+app.use(express.urlencoded({ limit: "16kb" }))
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
 export { app };
