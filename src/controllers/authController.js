d15f9292caedbedca29ef79484e2012f1fa13301
Mayank Bajaj <mayankbajaj672@gmail.com>
2025-12-25 08:55:12 +0530

    send verfify otp

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -103,3 +103,35 @@ export const logout = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 }
+
+// Send Verification OTP to the User's Email
+export const sendVerifyOtp = async (req, res) => {
+    try {
+        const { userId } = req.body;
+        const user = await userModel.findById(userId);
+        if (!user.isAccountVerified) {
+            return res.json({ success: false, message: "Account already verified"})
+        }
+
+        const otp = String(Math.floor(100000 + Math.random() * 900000))
+
+        user.verifyOtp = otp;
+        user.verifyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000
+
+        await user.save();
+
+        const mailOptions = {
+            from: process.env.EMAIL,
+            to: user.email,
+            subject: "Account Verification OTP",
+            text: `Your OTP is ${otp}. Verify your account using this OTP`
+        }
+
+        await transporter.sendMail(mailOptions)
+
+        res.json({ success: true, message: `Verification OTP sent on Email` })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
